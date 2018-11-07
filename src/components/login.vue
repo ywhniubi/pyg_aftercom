bc76fa75ada31f06a062587fcc81b23a8b50cde9
beer.ywh <778652070@qq.com>
2018-11-07 20:05:26 +0800

    first commit

---
+++ src/components/login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div  class="login">{{msg}}111</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: '就是你'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
