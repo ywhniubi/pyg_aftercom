a81ca628f2956f32a4bb7efa5a63277c65d14b86
beer.ywh <778652070@qq.com>
2018-11-10 16:25:01 +0800

    主页结构完成

--- src/components/home.vue
+++ src/components/home.vue
@@ -2,12 +2,66 @@
   <el-container>
   <el-header>
 <div class="logo"></div>
-<div class="desc">{{msg}}<a href="">退出</a></div>
+<div class="desc">{{msg}}<a  @click="exit" href="#">退出</a></div>
 <h1 class="title">电商后台管理系统</h1>
 
   </el-header>
   <el-container>
-    <el-aside width="200px">Aside</el-aside>
+    <el-aside width="200px"><el-row class="tac">
+  <el-col :span="24" >
+    <el-menu
+      collapse-transition
+      default-active="1-1"
+      class="el-menu-vertical-demo"
+      @open="handleOpen"
+      unique-opened
+      @close="handleClose"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b">
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>用户管理</span>
+        </template>
+          <el-menu-item index="1-1"><i class="el-icon-menu"></i>用户列表 </el-menu-item>
+      </el-submenu>
+         <el-submenu index="2">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>权限管理</span>
+        </template>
+            <el-menu-item index="2-1"><i class="el-icon-menu"></i>角色列表</el-menu-item>
+           <el-menu-item index="2-2"><i class="el-icon-menu"></i>权限列表</el-menu-item>
+      </el-submenu>
+          <el-submenu index="3">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>商品管理</span>
+        </template>
+         <el-menu-item index="3-1"><i class="el-icon-menu"></i>商品列表</el-menu-item>
+        <el-menu-item index="3-2"><i class="el-icon-menu"></i>分类参数</el-menu-item>
+          <el-menu-item index="3-3"><i class="el-icon-menu"></i>商品分类</el-menu-item>
+      </el-submenu>
+          <el-submenu index="4">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>订单管理</span>
+        </template>
+         <el-menu-item-group>
+             <el-menu-item index="4-1"><i class="el-icon-menu"></i>订单列表</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+          <el-submenu index="5">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>数据统计</span>
+        </template>
+         <el-menu-item index="4-1"><i class="el-icon-menu"></i>数据报表</el-menu-item>
+      </el-submenu>
+    </el-menu>
+  </el-col>
+</el-row></el-aside>
     <el-main>Main</el-main>
   </el-container>
 </el-container>
@@ -18,6 +72,18 @@ export default {
   data() {
     return {
       msg: '欢迎光临'
+    }
+  },
+  methods: {
+    exit() {
+      localStorage.removeItem('token')
+      this.$router.push('/login')
+    },
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath)
     }
   }
 }
@@ -92,6 +158,18 @@ body > .el-container {
 }
 #app {
   width: 100%;
+  height: 100%;
+}
+.el-col {
+  height: 100%;
+}
+.el-col ul {
+  height: 100%;
+}
+.el-col ul li {
+  padding-top: 20px;
+}
+.el-row {
   height: 100%;
 }
 </style>
